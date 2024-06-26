4022e39aca1f9c576d6781f4a46044393e58aff5
yusufazam225 <justforcoding225@gmail.com>
2024-06-26 10:19:51 +0530

    Koinx:Define Sample Schema

--- src/models/models.js
+++ src/models/models.js
@@ -3,8 +3,7 @@ const schema=new mongoose.schema({
     utc_time:{type:Date,required:true},
     operation:{type:String,enum:['buy','sell'],require:true},
     market: { type: String, required: true },
-    base_coin: { type: String, required: true },
-    quote_coin: { type: String, required: true },
+   user_id:{type:Number,required:true},
     amount: { type: Number, required: true },
     price: { type: Number, required: true }
 });
