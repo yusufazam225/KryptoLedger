f27e9748db1c496d6cde24b0d08bd75dec294284
yusufazam225 <justforcoding225@gmail.com>
2024-06-27 20:47:32 +0530

    koinx: added cors

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import dotenv from 'dotenv'
 import routes from './routes/routes.js'
 import connectDB from './db/index.js'
 import { swaggerUi, swaggerSpec } from './swagger.js'
+
+import cors from 'cors';
+
+const app=express();
+
 dotenv.config({
     path:'./.env'
 })
-const app=express();
 
+const corsOptions = {
+    origin: 'http://ec2-52-62-104-194.ap-southeast-2.compute.amazonaws.com:3000',
+    optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api',routes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
